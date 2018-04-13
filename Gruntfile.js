94ad165014ebfe5a6b40408c0f3d14979565ea80
Mario Schubert <mario.schubert@eran.de>
2018-04-13 11:13:30 +0200

    complete grunt svg workflow with svgstore, iconmoon not needed any more, renamed all svg to lower case

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@
 require("es5-shim");
 require("es6-shim");
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   grunt.loadNpmTasks("grunt-karma");
   grunt.loadNpmTasks("grunt-contrib-jshint");
   grunt.loadNpmTasks("grunt-contrib-uglify");
@@ -12,6 +12,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-contrib-connect");
   grunt.loadNpmTasks("grunt-jasmine-nodejs");
   grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks('grunt-svgstore');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
@@ -20,6 +21,31 @@ module.exports = function(grunt) {
       dist: require("./config/webpack/dist"),
       test: require("./config/webpack/test"),
       uncompressed: require("./config/webpack/uncompressed")
+    },
+
+    svgstore: {
+      options: {
+        prefix: '', // This will prefix each <g> ID
+        formatting: {
+          indent_size: 2
+        },
+        cleanup: true,
+        cleanupdefs: true,
+        svg: { // will add and override the the default xmlns="http://www.w3.org/2000/svg" attribute to the resulting SVG
+          "xmlns:dc": "http://purl.org/dc/elements/1.1/",
+          "xmlns:cc": "http://creativecommons.org/ns#",
+          "xmlns:rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+          "xmlns:svg": "http://www.w3.org/2000/svg",
+          "xmlns": "http://www.w3.org/2000/svg",
+          "xmlns:sodipodi": "http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd",
+          "xmlns:inkscape": "http://www.inkscape.org/namespaces/inkscape"
+        }
+      },
+      default: {
+        files: {
+          'src/icons/sir-trevor-icons.svg': ['public/images/icons/src/*.svg'],
+        }
+      }
     },
 
     "webpack-dev-server": {
@@ -77,12 +103,11 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.registerTask('default', ['svgstore', 'webpack:uncompressed', 'webpack:dist']);
+  grunt.registerTask('test', ['clean:all', 'jshint', 'karma', 'svgstore', 'test-integration']);
+  grunt.registerTask('test-integration', ['webpack:test', 'connect', 'jasmine_nodejs']);
+  grunt.registerTask('dev', ['svgstore', 'webpack-dev-server:start']);
   grunt.registerTask("default", ["webpack:uncompressed", "webpack:dist"]);
-  grunt.registerTask("test", ["clean:all", "karma", "test-integration"]);
-  grunt.registerTask("test-integration", [
-    "webpack:test",
-    "connect",
-    "jasmine_nodejs"
-  ]);
+
   grunt.registerTask("dev", ["webpack-dev-server:start"]);
 };
