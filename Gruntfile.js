7e651b99958974f7be0e414a69f2242b7a615d67
Mario Schubert <mario.schubert@eran.de>
2018-04-13 11:13:30 +0200

    complete grunt svg workflow with svgstore, iconmoon not needed any more

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,6 +48,31 @@ module.exports = function (grunt) {
       }
     },
 
+    svgstore: {
+      options: {
+        prefix: '', // This will prefix each <g> ID
+        formatting: {
+          indent_size: 2
+        },
+        cleanup: true,
+        cleanupdefs: true,
+        svg: { // will add and override the the default xmlns="http://www.w3.org/2000/svg" attribute to the resulting SVG
+          "xmlns:dc": "http://purl.org/dc/elements/1.1/",
+          "xmlns:cc": "http://creativecommons.org/ns#",
+          "xmlns:rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+          "xmlns:svg": "http://www.w3.org/2000/svg",
+          "xmlns": "http://www.w3.org/2000/svg",
+          "xmlns:sodipodi": "http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd",
+          "xmlns:inkscape": "http://www.inkscape.org/namespaces/inkscape"
+        }
+      },
+      default: {
+        files: {
+          'src/icons/sir-trevor-icons.svg': ['public/images/icons/src/*.svg'],
+        }
+      }
+    },
+
     "webpack-dev-server": {
       start: {
         webpack: require("./config/webpack/dev"),
