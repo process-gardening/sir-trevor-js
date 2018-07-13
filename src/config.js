5b42fc614742144a230fa0d49c2f1284601ca8be
Mario Schubert <mario.schubert@eran.de>
2018-07-13 18:49:05 +0200

    updated lodash and added debounce + findindex

--- src/config.js
+++ src/config.js
@@ -12,10 +12,10 @@ var drop_options = {
   html: `
     <div class="st-block__dropzone">
       <svg role="img" class="st-icon">
-        <use xlink:href="<%= config.defaults.iconUrl %>#<%= _.result(block, "icon_name") %>"/>
+        <use xlink:href="<%= config.defaults.iconUrl %>#<%= _.result(block, 'icon_name') %>"/>
       </svg>
       <p>
-        <%= i18n.t("general:drop", { block: "<span>" + _.result(block, "title") + "</span>" }) %>
+        <%= i18n.t("general:drop", { block: "<span>" + _.result(block, 'title') + "</span>" }) %>
       </p>
     </div>
   `,
